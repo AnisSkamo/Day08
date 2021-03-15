d8fec5ebdc893d7b4d04d82b0243b310f0d2dd55
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-15 20:33:44 +0100

    Add files via upload

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,1 +1,11 @@
+// Only change code below this line
+var total = 0;
 
+function funcWithArg(digit) {
+    return (digit + 9) / 5
+}
+console.log(funcWithArg(6));
+return total;
+// Only change code above this line
+console.log(total);
+module.exports = funcWithArg;
