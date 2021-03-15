700cd6b6237e40241f5253ef9f6d5df77f348640
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-15 22:07:28 +0100

    Add files via upload

--- ex12/src/index.js
+++ ex12/src/index.js
@@ -1,1 +1,8 @@
-
+// Only change code below this line
+function randomInteger(){
+    return (Math.random(0, 30) * 30);
+    Math.floor(Math.random(0, 30) * 30);
+}
+console.log(Math.round(randomInteger()))
+// Only change code above this line
+module.exports = randomInteger;
