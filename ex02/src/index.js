eb1d9757a423e50300b6b18436384f1481373e1e
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-15 19:03:14 +0100

    Add files via upload

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,1 +1,7 @@
-
+//Write code below this line
+function functionPrintInfo(parameter1, parameter2) {
+    console.log(parameter1, parameter2); 
+}
+functionPrintInfo("John" + " " + "23", "Michael" + " " + "43");
+//Write code above this line
+module.exports = functionPrintInfo;
