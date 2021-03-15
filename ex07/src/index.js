f068b7043a5920a6ecef842028031bb4dce39f9c
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-15 20:14:27 +0100

    Add files via upload

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,1 +1,19 @@
+// Only change code below this line
+var sum = 0;
+function addThree(num) {
+    addThree = sum + 3;
+}
+addThree(3);
 
+function addFive(num) {
+    addFive = addThree + 5;
+}
+addFive(5);
+console.log(addThree);
+console.log(addFive);
+
+// Only change code above this line
+module.exports = {
+    addThree,
+    addFive
+};
