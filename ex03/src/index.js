9d7d72fccd087d3632d78a755209010363df3ac8
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-15 19:27:42 +0100

    Add files via upload

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,1 +1,23 @@
-
+// Write code below this line
+var myGlobalVariable = 10;
+function function1 () {
+    console.log("notGlobalVariable", "5");
+}
+function1()
+// write code above this line
+function function2() {
+    var result = "";
+    if (typeof myGlobalVariable != "undefined") {
+        result += "myGlobalVariable: " + myGlobalVariable;
+    }
+    if (typeof notGlobalVariable != "undefined") {
+        result += "notGlobalVariable: " + notGlobalVariable;
+    }
+    console.log(result);
+}
+function1();
+function2();
+module.exports = {
+    function1,
+    function2
+};
