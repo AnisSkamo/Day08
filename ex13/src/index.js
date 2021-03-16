54947af3e1a0dfe65bb6ba536b1ee92c989ade50
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-16 10:06:37 +0100

    Add files via upload

--- ex13/src/index.js
+++ ex13/src/index.js
@@ -1,1 +1,9 @@
-
+// Only change code below this line
+function randomRangeNumber(minNumber, maxNumber) {
+    return Math.floor(Math.random() * Math.floor(maxNumber));
+};
+console.log(randomRangeNumber(7,14));
+randomRangeNumber();
+  // expected output: a number from 0 to <1
+// Only change code above this line
+module.exports = randomRangeNumber;
